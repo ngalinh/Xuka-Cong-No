9477b6c5fdb42acc074c1c0fe4ed75436acf4197
ngalinh <linh@basso.vn>
2026-06-08 17:45:47 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Xuka-congno.html'));
+});
+
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
